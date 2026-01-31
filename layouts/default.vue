d29d3517f50c58bce32cc3517d696b4f5cc40211
Nontachai Phosri <103272122+Gas-nontachai@users.noreply.github.com>
2026-01-31 19:39:37 +0700

    feat: favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,10 @@ useHead({
   titleTemplate: () => {
     return "Portfolio's Nonthachai";
   },
-  link: [{ rel: "icon", href: "@/assets/images/logo/portfolio.png" }],
+  link: [
+    { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+    { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+  ],
 });
 </script>
 
